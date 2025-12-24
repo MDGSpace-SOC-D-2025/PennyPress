159413d06bf339002bad99936965d2bddfe2c4fd
Tathagat <janaktathagat@gmail.com>
2025-12-24 00:18:53 +0000

    feat: Wrote unit tests with 100% line and branch coverage using foundry. (Closes #9)

--- src/PennyPress.sol
+++ src/PennyPress.sol
@@ -2,29 +2,32 @@
 pragma solidity ^0.8.19;
 
 contract PennyPress {
-
     mapping(bytes32 => uint256) public articlePrices;
     mapping(bytes32 => address) public articleCreators;
     mapping(bytes32 => mapping(address => bool)) public hasAccess;
 
     event ArticleRegistered(
-        bytes32 indexed articleId, 
+        bytes32 indexed articleId,
         string ipfsCid,
-        address indexed creator, 
+        address indexed creator,
         uint256 price
     );
     event AccessGranted(bytes32 indexed articleId, address indexed user);
 
-    function registerArticle(bytes32 articleId, uint256 price,  string calldata ipfsCid) external {
+    function registerArticle(
+        bytes32 articleId,
+        uint256 price,
+        string calldata ipfsCid
+    ) external {
         require(articlePrices[articleId] == 0, "Article ID already exists");
-        
+
         articlePrices[articleId] = price;
         articleCreators[articleId] = msg.sender;
         hasAccess[articleId][msg.sender] = true;
 
         emit ArticleRegistered(articleId, ipfsCid, msg.sender, price);
     }
-
+    
     function payToAccess(bytes32 articleId) external payable {
         uint256 price = articlePrices[articleId];
         require(price > 0, "Article does not exist");
@@ -36,11 +39,14 @@ contract PennyPress {
 
         (bool success, ) = payable(creator).call{value: msg.value}("");
         require(success, "Transfer failed");
-        
+
         emit AccessGranted(articleId, msg.sender);
     }
 
-    function checkAccess(bytes32 articleId, address user) external view returns (bool) {
+    function checkAccess(
+        bytes32 articleId,
+        address user
+    ) external view returns (bool) {
         return hasAccess[articleId][user];
     }
 }
---
+++ src/PennyPress.t.sol
@@ -0,0 +1,109 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {console} from "../lib/openzeppelin-contracts/lib/forge-std/src/console.sol";
+import { Test } from "../lib/openzeppelin-contracts/lib/forge-std/src/Test.sol";
+import {PennyPress} from "./PennyPress.sol";
+
+contract PennyPressTest is Test{ 
+
+    PennyPress public pennyPress;
+    address public Creator = makeAddr("creator");
+    address public User = makeAddr("user");
+    
+    event ArticleRegistered(
+        bytes32 indexed articleId, 
+        string ipfsCid,
+        address indexed creator, 
+        uint256 price
+    );
+
+    function setUp() public {
+        pennyPress = new PennyPress();
+
+    }
+
+    function testRegisterArticle() public{
+        bytes32 testArticleId = keccak256("TestArticleId");
+        uint256 testPrice = 0.1 ether;
+        string memory testIpfsID = "SomeTestingHash123";
+
+        vm.startPrank(Creator);
+
+        vm.expectEmit(true,true,true,true);
+        emit ArticleRegistered(testArticleId, testIpfsID, Creator, testPrice);
+
+        pennyPress.registerArticle(testArticleId, testPrice, testIpfsID);
+
+        vm.expectRevert("Article ID already exists");
+        pennyPress.registerArticle(testArticleId, testPrice, testIpfsID);
+        
+        assertEq(pennyPress.articlePrices(testArticleId) , testPrice);
+        assertEq(pennyPress.articleCreators(testArticleId), Creator);
+        assertEq(pennyPress.hasAccess(testArticleId,Creator) , true);
+        
+        vm.stopPrank();
+
+    }
+    
+    function testPayToAccess() public {
+        RevertingReceiver badCreator = new RevertingReceiver();
+
+        bytes32 testArticleId = keccak256("TestArticleId");
+        bytes32 badArticleId = keccak256("bad-article");
+        uint256 testPrice = 0.1 ether;
+        string memory testIpfsID = "SomeTestingHash123";
+
+        vm.startPrank(Creator);
+
+        vm.expectRevert("Article does not exist");
+        pennyPress.payToAccess(testArticleId);
+
+        vm.deal(Creator, 10 ether);
+        pennyPress.registerArticle(testArticleId, testPrice, testIpfsID);
+
+        vm.expectRevert("Insufficient payment");
+        pennyPress.payToAccess{value: 0.05 ether}(testArticleId);
+
+        vm.expectRevert("Already has access");
+        pennyPress.payToAccess{value: 0.1 ether}(testArticleId);
+
+        vm.stopPrank();
+
+        vm.startPrank(address(badCreator));
+        pennyPress.registerArticle(badArticleId, 0.8 ether, testIpfsID);
+        vm.stopPrank();
+
+        vm.startPrank(User);
+
+        vm.deal(User, 10 ether);
+        pennyPress.payToAccess{value: 0.1 ether}(testArticleId);
+
+        vm.expectRevert("Transfer failed");
+        pennyPress.payToAccess{value: 0.8 ether}(badArticleId);
+
+        vm.stopPrank();
+    }
+    function testCheckAccess() public{
+        bytes32 testArticleId = keccak256("TestArticleId");
+        uint256 testPrice = 0.1 ether;
+        string memory testIpfsID = "SomeTestingHash123";
+
+        vm.startPrank(Creator);  
+
+        pennyPress.registerArticle(testArticleId, testPrice, testIpfsID);
+        bool creatorResult = pennyPress.checkAccess(testArticleId, Creator);
+        bool userResult = pennyPress.checkAccess(testArticleId, User);
+        
+        vm.stopPrank();
+
+                assertEq(creatorResult, true);
+        assertEq(userResult, false);
+    }
+}
+
+contract RevertingReceiver {
+    receive() external payable {
+        revert("I do not accept ETH");
+    }
+}
